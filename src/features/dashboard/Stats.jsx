6ac07d2ca752c141efe9c5ffa23a35eee6df3b0c
AzyzXXIII <azizhcini001@gmail.com>
2025-03-13 19:44:40 +0100

    fixing dashboard

--- src/features/dashboard/Stats.jsx
+++ src/features/dashboard/Stats.jsx
@@ -9,19 +9,9 @@ import Stat from "./Stat";
 //import { formatCurrency } from "../../utils/helpers";
 
 function Stats({ reports, confirmedStays, numEmployees, numHighSeverity }) {
-  // 1.
   const numReports = reports.length;
 
-  // 2.
-  //const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
-
-  // 3.
   const checkins = confirmedStays.length;
-
-  // 4.
-  /* const occupation =
-    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
-    (numDays * cabinCount); */
 
   return (
     <>
