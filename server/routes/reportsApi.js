2bf46881a74c89ab7f7dcf7d8a4814680a9a14d4
AzyzXXIII <azizhcini001@gmail.com>
2025-03-21 22:43:32 +0100

    cleaning the code

--- server/routes/reportsApi.js
+++ server/routes/reportsApi.js
@@ -1,16 +1,13 @@
 import express from "express";
-import pool from "../config/db.js"; // Import the database connection
+import pool from "../config/db.js";
 
 const router = express.Router();
 
-/**
- * GET all observations from the database
- */
 router.get("/observations", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM observation;");
     console.log(result.rows);
-    res.json(result.rows); // Return the fetched data
+    res.json(result.rows);
   } catch (error) {
     console.error("Error fetching observations:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
