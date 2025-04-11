54c9445e1f0bcd8cbb6c55908db9d9676e4d81e2
AzyzXXIII <azizhcini001@gmail.com>
2025-04-11 01:29:32 +0100

    fixing tailwind

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // make sure your files are included
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
