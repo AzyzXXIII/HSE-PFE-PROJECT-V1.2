34c0a3fef1dabaf3fd9cc395c1227e33b8b92bcd
AzyzXXIII <azizhcini001@gmail.com>
2025-05-15 23:41:21 +0100

    debugging

--- server/models/reportModel.js
+++ server/models/reportModel.js
@@ -99,7 +99,6 @@ export const fetchRecentReports = async () => {
     tables.map(async (table) => {
       console.log(`✅ Fetching from table: ${table}`);
 
-      // Check if the 'time' column exists in the table
       const checkTimeColumnQuery = `
         SELECT column_name
         FROM information_schema.columns
